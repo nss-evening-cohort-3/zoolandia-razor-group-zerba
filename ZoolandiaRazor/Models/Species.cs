c850165cf51b6e1880a3f9979c8d60c5fa5e17c7
Bradley Guthrie <guppy96@gmail.com>
2016-10-08 10:39:20 -0500

    animal species classes

--- ZoolandiaRazor/Models/Species.cs
+++ ZoolandiaRazor/Models/Species.cs
@@ -10,12 +10,16 @@ namespace ZoolandiaRazor.Models
 {
     public class Species
     {
+        [Key]
         public int SpeciesId { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string CommonName { get; set; }
 
+        [Required]
         public string ScientificName { get; set; }
       
     }
