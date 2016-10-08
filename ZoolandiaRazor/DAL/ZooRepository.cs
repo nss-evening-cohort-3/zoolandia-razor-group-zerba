8b395c9465b2e8a7f65fc553540a718ed7df1274
JZorvan <jzorvan@gmail.com>
2016-10-08 10:39:31 -0700

    kremoved plurals to match brad's context

--- ZoolandiaRazor/DAL/ZooRepository.cs
+++ ZoolandiaRazor/DAL/ZooRepository.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ZoolandiaRazor.Models;
 
 namespace ZoolandiaRazor.DAL
 {
     public class ZooRepository
     {
+        public ZooContext Context { get; set; }
+
+        public ZooRepository()
+        {
+            Context = new ZooContext();
+        }
+
+        public ZooRepository(ZooContext _context)
+        {
+            Context = _context;
+        }
+
+        public List<Animal> GetAnimals()
+        {
+            return Context.Animal.ToList();
+        }
+
+        public List<Habitat> GetHabitats()
+        {
+            return Context.Habitat.ToList();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return Context.Employee.ToList();
+        }
+
     }
 }
