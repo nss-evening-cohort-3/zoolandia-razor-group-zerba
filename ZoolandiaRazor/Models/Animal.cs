3b3b0b7fe0d6f15b4bc52427f6cf693e241515f6
Bradley Guthrie <guppy96@gmail.com>
2016-10-07 19:08:02 -0500

    erd and two classes

---
+++ ZoolandiaRazor/Models/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZoolandiaRazor.Models
+{
+    public class Animal
+    {
+        public int AnimailId { get; set; }
+
+        public String  Name { get; set; }
+
+        public int Habitat { get; set; }
+
+        public int Species { get; set; }
+
+        public int Age { get; set; }
+
+
+    }
+}
