636b6181f66dde9ac29096a50728edb299332d71
ZachParris <zacharyjparris@gmail.com>
2016-10-12 17:34:45 -0500

    built view index

--- ZoolandiaRazor/Controllers/HabitatController.cs
+++ ZoolandiaRazor/Controllers/HabitatController.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZoolandiaRazor.DAL;
+using ZoolandiaRazor.Models;
 
 namespace ZoolandiaRazor.Controllers
 {
     public class HabitatController : Controller
     {
+        private ZooRepository repo = new ZooRepository();
+
         // GET: Habitat
         public ActionResult Index()
         {
+            ViewBag.Habitats = repo.GetHabitats();
+
             return View();
         }
 
         // GET: Habitat/Details/5
         public ActionResult Details(int id)
         {
+            Habitat picked_habitat = repo.FindHabitatById(id);
+            ViewBag.Habitats = picked_habitat;
             return View();
         }
 
